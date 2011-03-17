c177703f3a12dec367b1f1ba8485bd3ec020177d
Scyptnex Koralis <scyptnex@hotmail.com>
2011-03-17 12:09:39 +1100

    Big commit containing all old files

---
+++ comp5426/collapse_blocking_template.c
@@ -0,0 +1,111 @@
+
+...
+
+#define BUF_SIZE 1024
+
+
+
+
+int main(int argc, char **argv) {
+
+
+
+    ...
+
+
+    if (numprocs == 1) {
+        /* trivial single CPU case */
+
+        ...
+
+    } else if (myid == 0) {
+        /* server case */
+
+        ...
+
+
+	/* read a block of chars to buf */
+        read = buf_read(buf, buf + BUF_SIZE);
+
+        /* initial allocation */
+	allocatedJobs = 0;
+        for(i = 0; i < numprocs - 1; i++) {
+	      if (read){
+		/* allocate a job to proc i+1 */
+	          
+                ...
+
+                allocatedJobs++;
+
+                /* read a block, but if the last read was a short item count, don't try to read again */
+                
+                ...
+
+	      }
+              else{
+		/* send a zero length array to terminate the worker */
+
+                ...
+
+ 
+            }
+        }
+
+        /* while there is any job to be allocated or received */
+        while(allocatedJobs) {                             
+            /*after sending jobs, we open a blocking socket and waite for return*/
+      
+            ...
+
+            /* status.MPI_SOURCE contains the sending process ID. We obtain the value received, 
+               and add them to the result */           
+
+            ...
+
+	    allocatedJobs--;
+ 
+            if (read){ 
+	      /* Allocate a new job for it */
+
+              ...
+	      allocatedJobs++;
+              /* read a block, but if the last read was a short item count, don't try to read again */
+                
+              ...
+
+	    }
+            else{
+              /* send a zero length array to terminate the worker */
+
+              ...
+
+            }
+        }
+    } else {
+        /* slave processor */
+ 
+        ...
+
+    }
+
+    if (myid == 0) {
+      /* print the output */
+
+      ...
+ 
+
+    }
+
+    ...
+
+}
+
+
+int buf_read(char * buf, const char * end) {
+    int count;
+    count = fread(buf, 1, end - buf, stdin);
+    if (count > 0 && i[count - 1] == '\n') {
+        --count;
+    }
+    return count;
+}
